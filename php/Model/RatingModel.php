427b13464023a01707705121419adab819f1e772
ercihkay <ercihkay@students.zhaw.ch>
2021-04-14 21:15:05 +0200

    #17 ratioColorsRate() function and helperFunction added

--- php/Model/RatingModel.php
+++ php/Model/RatingModel.php
@@ -16,8 +16,16 @@
          * This function calculates the penalties for wrong color rates of a in the database defined color for a word
          * @Return: int penaltiesForColorRate
          */
-        function ratioColorsRate(){
+        function ratioColorsRate($word, $sketchIndx){
             list($blackCounter, $redCounter, $greenCounter, $blueCounter, $yellowCounter, $orangeCounter) = $this->imageProcessingController->pixelCount();
+            $primaryColorCounter = 0;
+            $secondaryColorCounter = 0;
+            $primaryOptimalColorRatio = $this->database->executeQuery("SELECT primaryColorRatio FROM tbl_word WHERE word = " .$word);
+            $secondaryOptimalColorRatio = $this->database->executeQuery("SELECT secondaryColorRatio FROM tbl_word WHERE word = " .$word);
+            $primaryColor = $this->database->executeQuery("SELECT primaryColor FROM tbl_word WHERE word = " .$word);
+            $secondaryColor = $this->database->executeQuery("SELECT secondaryColor FROM tbl_word WHERE word = " .$word);
+            $primaryColorCounterNum = $this->setupColorCounter($blackCounter, $redCounter, $greenCounter, $blueCounter, $yellowCounter, $orangeCounter, $primaryColor);
+            $secondaryColorCounterNum = $this->setupColorCounter($blackCounter, $redCounter, $greenCounter, $blueCounter, $yellowCounter, $orangeCounter, $secondaryColor);
         }
         
         /**
@@ -31,5 +39,31 @@
          * @Return: int penaltieTotal
          */
         function collectPenalties(){}
+
+        function setupColorCounter($blackCounter, $redCounter, $greenCounter, $blueCounter, $yellowCounter, $orangeCounter, $colorToSelect){
+            $colorCounter = 0;
+            switch ($colorToSelect){
+                case "black":
+                    $colorCounter = $blackCounter;
+                    break;
+                case "red":
+                    $colorCounter = $redCounter;
+                    break;
+                case "green":
+                    $colorCounter = $greenCounter;
+                    break;
+                case "blue":
+                    $colorCounter = $blueCounter;
+                    break;
+                case "yellow":
+                    $colorCounter = $yellowCounter;
+                    break;
+                case "orange":
+                    $colorCounter = $orangeCounter;
+                    break;
+            }
+            return $colorCounter;
+        }
+
     }
 ?>

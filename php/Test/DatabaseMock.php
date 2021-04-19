a6f3cc349f1f04e14fc861aea3cdfbaf86911a8e
Roman <bernero1@students.zhaw.ch>
2021-04-19 21:38:00 +0200

    Changes Kaya
    Changes from class to static function

--- php/Test/DatabaseMock.php
+++ php/Test/DatabaseMock.php
@@ -3,107 +3,107 @@
 include_once("Database.php");
 
 class DatabaseMock{
-    private $lobbyIndex;
-    private $playerIndex;
-    private $gameIndex;
-    private $roundIndex;
-    private $wordpoolIndex;
-    private $wordIndex;
-    private $sketchIndex;
+    private static $lobbyIndex;
+    private static $playerIndex;
+    private static $gameIndex;
+    private static $roundIndex;
+    private static $wordpoolIndex;
+    private static $wordIndex;
+    private static $sketchIndex;
 
     /**
      * Create a mock for a whole game
      */
-    public function createDatabaseMock(){
-        $this->createWordpool();
-        $this->createWord();
-        $this->createWordpoolWord();
-        $this->createPlayers();
-        $this->createLobby();
-        $this->createLobbyPlayer();
-        $this->createLobbyWordpool();
-        $this->createGame();
-        $this->createRound();
-        $this->createGameRound();
-        $this->createRoundPlayer();
+    public static function createDatabaseMock(){
+        self::createWordpool();
+        self::createWord();
+        self::createWordpoolWord();
+        self::createPlayers();
+        self::createLobby();
+        self::createLobbyPlayer();
+        self::createLobbyWordpool();
+        self::createGame();
+        self::createRound();
+        self::createGameRound();
+        self::createRoundPlayer();
     }
 
-    private function createWordpool(){
+    private static function createWordpool(){
         $query = "INSERT INTO tbl_wordpool (word) Values ('Halunken')";
         CorbleDatabase::executeInsertQuery($query);
 
         $query = "SELECT indx FROM tbl_wordpool WHERE word='Halunken'";
-        $this->wordpoolIndex = CorbleDatabase::executeQuery($query);
+        self::$wordpoolIndex = CorbleDatabase::executeQuery($query);
     }
 
-    private function createWord(){
+    private static function createWord(){
         $query = "INSERT INTO tbl_word (primaryColor, secundaryColor, primaryColorRatio, secundaryColorRatio, word) 
             Values ('red', 'blue', 0.5, 0.5, 'Mackie Messer')";
         CorbleDatabase::executeInsertQuery($query);
 
         $query = "SELECT indx FROM tbl_word WHERE name='Mackie Messer'";
-        $this->wordIndex= CorbleDatabase::executeQuery($query);
+        self::$wordIndex= CorbleDatabase::executeQuery($query);
     }
 
-    private function createWordpoolWord(){
+    private static function createWordpoolWord(){
         $query = "INSERT INTO tbl_wordpool_word (fk_word_index_wordpool_word, fk_wordpool_indx_wordpool_word) 
-            Values (". $this->wordIndex . ",". $this->wordpoolIndex .")";
+            Values (". self::$wordIndex . ",". self::$wordpoolIndex .")";
         CorbleDatabase::executeInsertQuery($query);
     }
 
-    private function createPlayers(){
+    private static function createPlayers(){
         $query = "INSERT INTO tbl_player (name) Values ('Gino')";
         CorbleDatabase::executeInsertQuery($query);
 
         $query = "SELECT indx FROM tbl_player WHERE name='Gino'";
-        $this->playerIndex = CorbleDatabase::executeQuery($query);
+        self::$playerIndex = CorbleDatabase::executeQuery($query);
     }
 
-    private function createLobby(){
+    private static function createLobby(){
         $query = "INSERT INTO tbl_lobby (votetime, drawtime, maxplayer, fk_player_index_lobby, state, joincode, starttime) 
-                Values (10, 10, 5, " . $this->playerIndex . ", 'Start', 1, 1617604745)";
+                Values (10, 10, 5, " . self::$playerIndex . ", 'Start', 1, 1617604745)";
         CorbleDatabase::executeInsertQuery($query);
 
         $query = "SELECT indx FROM tbl_Lobby WHERE joincode = 1";
-        $this->lobbyIndex= CorbleDatabase::executeQuery($query);
+        self::$lobbyIndex= CorbleDatabase::executeQuery($query);
     }
 
-    private function createLobbyPlayer(){
+    private static function createLobbyPlayer(){
         $query = "INSERT INTO tbl_lobby_lobby (fk_lobby_index_Lobby_player, fk_player_index_lobby_player, partyLeader) 
-            VALUES ($this->lobbyIndex, $this->playerIndex, 1)";
+            VALUES (".self::$lobbyIndex.", ".self::$playerIndex.", 1)";
         CorbleDatabase::executeInsertQuery($query);
     }
 
-    private function createLobbyWordpool(){
+    private static function createLobbyWordpool(){
         $query = "INSERT INTO tbl_lobby_wordpool(fk_lobby_index_lobby_wordpool, fk_wordpool_indx_lobby_wordpool) 
-            VALUES ($this->lobbyIndex, $this->wordpoolIndex)";
+            VALUES (".self::$lobbyIndex.", ".self::$wordpoolIndex.")";
         CorbleDatabase::executeInsertQuery($query);
     }
 
-    private function createGame(){
-        $query = "INSERT INTO tbl_game (fk_lobby_indx_game) alues (" . $this->lobbyIndex . ")";
+    private static function createGame(){
+        $query = "INSERT INTO tbl_game (fk_lobby_indx_game) alues (" . self::$lobbyIndex . ")";
         CorbleDatabase::executeInsertQuery($query);
 
-        $query = "SELECT indx FROM tbl_game WHERE fk_lobby_indx_game = " . $this->lobbyIndex;
-        $this->gameIndex = CorbleDatabase::executeQuery($query);
+        $query = "SELECT indx FROM tbl_game WHERE fk_lobby_indx_game = " . self::$lobbyIndex;
+        self::$gameIndex = CorbleDatabase::executeQuery($query);
     }
 
-    private function createRound(){
-        $query = "INSERT INTO tbl_round (fk_word_indx_round) alues (" . $this->wordIndex . ")";
+    private static function createRound(){
+        $query = "INSERT INTO tbl_round (fk_word_indx_round) alues (" . self::$wordIndex . ")";
         CorbleDatabase::executeInsertQuery($query);
 
-        $query = "SELECT indx FROM tbl_round WHERE fk_word_indx_round = " . $this->lobbyIndex;
-        $this->roundIndex= CorbleDatabase::executeQuery($query);
+        $query = "SELECT indx FROM tbl_round WHERE fk_word_indx_round = " . self::$lobbyIndex;
+        self::$roundIndex= CorbleDatabase::executeQuery($query);
     }
 
-    private function createGameRound(){
+    private static function createGameRound(){
         $query = "INSERT INTO tbl_game_round (fk_game_index_game_round, fk_round_indx_game_round) 
-            Values (". $this->gameIndex . ",". $this->roundIndex .")";
+            Values (". self::$gameIndex . ",". self::$roundIndex .")";
         CorbleDatabase::executeInsertQuery($query);
     }
-    private function createRoundPlayer(){
+    private static function createRoundPlayer(){
         $query = "INSERT INTO tbl_round_player (fk_round_indx_round_player, fk_player_indx_round_player) 
-            Values (". $this->roundIndex . ",". $this->playerIndex .")";
+            Values (". self::$roundIndex . ",". self::$playerIndex .")";
         CorbleDatabase::executeInsertQuery($query);
     }
 

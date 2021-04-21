39e67f5c43d46b665e649f5301c90a6d4516a290
ercihkay <ercihkay@students.zhaw.ch>
2021-04-21 21:48:36 +0200

    #27 sketchRateOfPlayer() method created

--- php/Controller/RoundController.php
+++ php/Controller/RoundController.php
@@ -1,9 +1,11 @@
 <?php
     class RoundController{
         //Construktur noch zu implementieren
-        
+
         public function rateSketch($sketchIndx){}
 
         public function selectRandomWord($category){}
+
+        public function sketchRateOfPlayer(){}
     }
 ?>

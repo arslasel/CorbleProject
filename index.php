7a0092095214f615ee5f9427fdc7bda69dd47d96
piologin <piologin@students.zhaw.com>
2021-05-18 21:42:35 +0200

    #27 Added first version of playable game

--- index.php
+++ index.php
@@ -24,6 +24,7 @@ session_start();
     <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
     <script src="js/init.js"></script>
+    <script>window.location.replace("https://corble.ch/php/View/LobbyView.php")</script>
 
 </head>
 
